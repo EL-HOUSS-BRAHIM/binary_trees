fc43386a7439e54b081a30f96f72f6a0b747733d
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2024-01-30 05:19:26 -0800

    binary

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,20 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_height - Helper function to calculate the height of a binary tree.
+ * @tree: A pointer to the root node of the tree.
+ *
+ * Return: The height of the tree.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+size_t left_height, right_height;
+if (tree == NULL)
+return (0);
+left_height = binary_tree_height(tree->left);
+right_height = binary_tree_height(tree->right);
+return (1 + (left_height > right_height ? left_height : right_height));
+}
+/**
  * binary_tree_is_perfect - Checks if a binary tree is perfect.
  * @tree: A pointer to the root node of the tree.
  *
