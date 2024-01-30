910f9eacb72b4954a681b7c0d7173f327579e44e
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2024-01-30 02:57:56 -0800

    binary_trees

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,9 +10,8 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
-
-    /* Allocate memory for the new node */
+binary_tree_t *new_node;
+/* Allocate memory for the new node */
     new_node = malloc(sizeof(binary_tree_t));
     if (new_node == NULL)
         return (NULL);
